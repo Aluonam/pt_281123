0a6bfd5ed3f8fa2aac5f2ae11adbb88ddc4680df
Paula Cano <paulacanoroman@gmail.com>
2023-11-29 11:46:51 +0100

    feat: delete useEffect, go through the object to get only the title
    ya que quiero que solo se ejecute la llamada al clicar el botón

--- pages/components/APIcall.jsx
+++ pages/components/APIcall.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const APIcall = () => {
 
     const [dataAPI, setDataAPI] = useState([])
     const [idWriteByUser, setIdWriteByUser] = useState(1)
 
-    useEffect(() => {
+    
       
-        const request = async (idTask)=>{
+    const request = async (idTask)=>{
             try{
                 const url = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${idTask}`);
                 const data = await url.json();
-                setDataAPI(data)
+                setDataAPI(data[0].title)
             } catch(err){err,"error detected"}
         }
-        request()
-    }, [])
+  
     
   return (
     <>
     <h3>Escribe el id de la tarea:</h3>
     <input type='number' onChange={(e)=>{setIdWriteByUser(e.target.value)}}></input>
-    <button onClick={()=>{}}>Muestra la tarea</button>
+    <button onClick={()=>{request(idWriteByUser)}}>Muestra la tarea</button>
     {dataAPI}
     </>
   )
