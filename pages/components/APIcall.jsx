edb76b2910705a21b5260d801ac0ba877237ff88
Paula Cano <paulacanoroman@gmail.com>
2023-11-29 11:40:51 +0100

    feat: call skeleton with useState useEffect try catch input and button

--- pages/components/APIcall.jsx
+++ pages/components/APIcall.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const APIcall = () => {
+
+    const [dataAPI, setDataAPI] = useState([])
+
+    useEffect(() => {
+      
+        const request = async (idTask)=>{
+            try{
+                const url = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${idTask}`);
+                const data = await url.json();
+                setDataAPI(data)
+            } catch(err){err,"error detected"}
+        }
+        request()
+    }, [])
+    
   return (
-    <div>APIcall</div>
+    <>
+    <h3>Escribe el id de la tarea:</h3>
+    <input type='number'></input>
+    <button onClick={()=>{}}>Muestra la tarea</button>
+    {dataAPI}
+    </>
   )
 }
 
